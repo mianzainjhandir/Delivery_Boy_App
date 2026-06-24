ff1e4fc0dadc4cfef869130ade5eaf09e63fbeeb
Bilal Saeed <mianzainjh@gmail.com>
2026-06-24 17:05:44 +0500

    Now i am going to create delivery_map_screen.dart.......

---
+++ lib/screens/delivery_map_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class DeliveryMapScreen extends StatelessWidget {
+  const DeliveryMapScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
